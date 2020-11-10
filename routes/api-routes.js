836eb2328b684ec1eeff8f9d6a990defef2aa101
dbray7 <devenbray@gmail.com>
2020-11-09 22:32:01 -0500

    added and fixed routes and server.js

---
+++ routes/api-routes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const store = require("../db/notes");
+
+// GET responds with all notes from the database
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+router.post("/notes", (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+// DELETE deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", function (req, res) {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+module.exports = router;
