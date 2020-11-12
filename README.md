f2711db1163c873d19dc59a9ec351810d21c94cf
dbray7 <devenbray@gmail.com>
2020-11-11 20:10:07 -0500

    Everything done except README

---
+++ README.md
@@ -0,0 +1,12 @@
+Summary
+This simple note taking application allows you to save notes with a title and plain text. Powered by ExpressJS, the Note Taker app makes creating, viewing, and deleting notes just a second!
+
+Prerequisites
+NodeJS
+Installing
+Clone the repository to your local development environment.
+
+git clone 
+Navigate to the developer-profile-generator folder using the command prompt.
+
+Run npm install to install all dependencies. To use the application locally, run node server.js in your CLI, and then open http://localhost:3000 in your preferred browswer. The Note Taker app is live on Heroku for you to use as well.
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,20 @@
 var path = require("path");
-var router = require("express").Router();
+var express = require('express');
+var router = express.Router();
+
 
 // "/notes" responds with the notes.html file
 router.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile('notes.html', {
+        root: path.join(__dirname, '../public')
+    })
 });
 
 // All other routes respond with the index.html file
-router.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/", function (req, res) {
+    res.sendFile('index.html', {
+        root: path.join(__dirname, '../public')
+    })
 });
 
 module.exports = router;
